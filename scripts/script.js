e040a646c572459d2854cc1420bf5da6cedb1b85
MeirOverferst <meiroverferst@gmail.com>
2019-01-09 10:42:37 +0100

    deleted the firat index.js  file

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-const assert = require(`assert`);
-describe(`My suite`, () => {
-    describe(`Just a test`, () => {
-        it(`should return true`, () => {
-            assert, equal(5, 5);
-        })
-    })
-
-})
